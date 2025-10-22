a0789ed926c6afa132b6b815e9fa7caa51882a76
Audyari Wiyono <audy123ari@gmail.com>
2025-10-22 12:08:23 +0700

    Full Code Todolist

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,230 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon
-            // src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    static String fileName;
+    static ArrayList<String> todoLists;
+    static boolean isEditing = false;
+    static Scanner input;
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        todoLists = new ArrayList<>();
+        input = new Scanner(System.in);
+
+        String filePath = System.console() == null ? "/src/todolist.txt" : "/todolist.txt";
+        fileName = System.getProperty("user.dir") + filePath;
+
+        while (true) {
+            showMenu();
+
         }
     }
+
+    static void clearScreen(){
+        try {
+            final String os = System.getProperty("os.name");
+            if (os.contains("Windows")) {
+                // clear screen untuk windows
+                new ProcessBuilder("cmd", "/c", "cls")
+                        .inheritIO()
+                        .start()
+                        .waitFor();
+            } else {
+                // clear screen untuk Linux, Unix, Mac
+                Runtime.getRuntime().exec("clear");
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (final Exception e) {
+            System.out.println("Error karena: " + e.getMessage());
+        }
+    }
+
+    static void showMenu() throws FileNotFoundException {
+        System.out.println("=== TODO LIST APP ===");
+        System.out.println("[1] Lihat Todo List");
+        System.out.println("[2] Tambah Todo List");
+        System.out.println("[3] Edit Todo List");
+        System.out.println("[4] Hapus Todo List");
+        System.out.println("[0] Keluar");
+        System.out.println("---------------------");
+        System.out.print("Pilih menu> ");
+
+        String selectedMenu = input.nextLine();
+
+        switch (selectedMenu) {
+            case "1" -> showTodoList();
+            case "2" -> addTodoList();
+            case "3" -> editTodoList();
+            case "4" -> deleteTodoList();
+            case "0" -> System.exit(0);
+            default -> {
+                System.out.println("Kamu salah pilih menu!");
+                backToMenu();
+            }
+        }
+    }
+
+    static void backToMenu() {
+        System.out.println();
+        System.out.print("Tekan [Enter] untuk kembali..");
+        input.nextLine();
+        clearScreen();
+    }
+
+    static void readTodoList() throws FileNotFoundException {
+        try {
+            File file = new File(fileName);
+
+            Scanner fileReader = new Scanner(file);
+
+            todoLists.clear();
+            while (fileReader.hasNextLine()) {
+                String data = fileReader.nextLine();
+                todoLists.add(data);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error karena: " + e.getMessage());
+        }
+
+    }
+
+    static void showTodoList() throws FileNotFoundException {
+        clearScreen();
+        readTodoList();
+
+        if (!todoLists.isEmpty()) {
+            System.out.println("TODO LIST:");
+            int index = 0;
+            for (String data : todoLists) {
+                System.out.printf("[%d] %s%n", index, data);
+                index++;
+            }
+        } else {
+            System.out.println("Tidak ada data!");
+        }
+
+        if (!isEditing) {
+            backToMenu();
+        }
+    }
+
+    static void addTodoList() {
+        clearScreen();
+
+        System.out.println("Apa yang ingin kamu kerjakan?");
+        System.out.print("Jawab: ");
+        String newTodoList = input.nextLine();
+
+        try {
+            // tulis file
+            FileWriter fileWriter = new FileWriter(fileName, true);
+            fileWriter.append(String.format("%s%n", newTodoList));
+            fileWriter.close();
+
+            System.out.println("Berhasil ditambahkan!");
+        } catch (IOException e) {
+            System.out.println("Terjadi kesalahan karena: " + e.getMessage());
+        }
+        backToMenu();
+    }
+
+    static void editTodoList() throws FileNotFoundException {
+        isEditing = true;
+        showTodoList();
+
+        try {
+            System.out.println("-----------------");
+            System.out.print("Pilih Indeks> ");
+            int index = Integer.parseInt(input.nextLine());
+
+            if (index > todoLists.size()) {
+                throw new IndexOutOfBoundsException("Kamu memasukkan data yang salah!");
+            } else {
+
+                System.out.print("Data baru: ");
+                String newData = input.nextLine();
+
+                // update data
+                todoLists.set(index, newData);
+
+                System.out.println(todoLists.toString());
+
+                try {
+                    FileWriter fileWriter = new FileWriter(fileName, false);
+
+                    // write new data
+                    for (String data : todoLists) {
+                        fileWriter.append(String.format("%s%n", data));
+                    }
+                    fileWriter.close();
+
+                    System.out.println("Berhasil diubah!");
+                } catch (IOException e) {
+                    System.out.println("Terjadi kesalahan karena: " + e.getMessage());
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        isEditing = false;
+        backToMenu();
+    }
+
+    static void deleteTodoList() throws FileNotFoundException {
+        isEditing = true;
+        showTodoList();
+
+        System.out.println("-----------------");
+        System.out.print("Pilih Indeks> ");
+        int index = Integer.parseInt(input.nextLine());
+
+        try {
+            if (index > todoLists.size()) {
+                throw new IndexOutOfBoundsException("Kamu memasukkan data yang salah!");
+            } else {
+
+                System.out.println("Kamu akan menghapus:");
+                System.out.printf("[%d] %s%n", index, todoLists.get(index));
+                System.out.println("Apa kamu yakin?");
+                System.out.print("Jawab (y/t): ");
+                String jawab = input.nextLine();
+
+                if (jawab.equalsIgnoreCase("y")) {
+                    todoLists.remove(index);
+
+                    // tulis ulang file
+                    try {
+                        FileWriter fileWriter = new FileWriter(fileName, false);
+
+                        // write new data
+                        for (String data : todoLists) {
+                            fileWriter.append(String.format("%s%n", data));
+                        }
+                        fileWriter.close();
+
+                        System.out.println("Berhasil dihapus!");
+                    } catch (IOException e) {
+                        System.out.println("Terjadi kesalahan karena: " + e.getMessage());
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        isEditing = false;
+        backToMenu();
+    }
+
 }
+
+
